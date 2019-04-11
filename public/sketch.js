24b0d479d39dbef7217be663367f2e104174276c
tom <powerth@tcd.ie>
2019-04-11 14:42:18 +0200

    add new graphics

--- public/sketch.js
+++ public/sketch.js
@@ -11,111 +11,127 @@ const BUFF_SIZE = 8;
 
 function getAverageArray(array) {
   var sum = 0;
-  for( var i = 0; i < array.length; i++ ){
-      sum += array[i]; //don't forget to add the base
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i]; //don't forget to add the base
   }
 
-  return sum/array.length;
+  return sum / array.length;
 }
 
 
 
 osc.on('/level', message => {
   val = message.args[0];
-     if(message.args[0]*100 < 1.0){
-      colorIndex = 0
-    } else if( message.args[0]*100 < 10.0 && message.args[0]*100 >= 1.0) {
-      colorIndex = 1;
+  if (message.args[0] * 100 < 1.0) {
+    colorIndex = 0
+  } else if (message.args[0] * 100 < 10.0 && message.args[0] * 100 >= 1.0) {
+    colorIndex = 1;
 
-    } else if (message.args[0]*100 > 10.0){
-      colorIndex = 2;
+  } else if (message.args[0] * 100 > 10.0) {
+    colorIndex = 2;
 
-    }
+  }
 })
 
 osc.on('/db', message => {
   let av = 0;
-    if(averageBuff.length < BUFF_SIZE) {
-            averageBuff.push(parseFloat(message.args[0]));      
+  if (averageBuff.length < BUFF_SIZE) {
+    averageBuff.push(parseFloat(message.args[0]));
 
-    } else {
-      averageBuff.shift();
-      averageBuff.push(parseFloat(message.args[0]));      
-    }
-    av = Math.round(getAverageArray(averageBuff))
-    dbString = av + ' dB'
+  } else {
+    averageBuff.shift();
+    averageBuff.push(parseFloat(message.args[0]));
+  }
+  av = Math.round(getAverageArray(averageBuff))
+  dbString = av + ' dB'
 
-    let k=10;
-    Object.keys(audioStatus).forEach(function(key) {
-     if(av > key) {
-       k = key;
+  if (av > 60) {
+    actRandomSeed = random(100000);
+
+  }
+
+  let k = 10;
+  Object.keys(audioStatus).forEach(function (key) {
+    if (av > key) {
+      k = key;
     }
-    });
-    status = "You are as loud as a " + audioStatus[k];
-    console.log(status);
+  });
+  status = "You are as loud as a " + audioStatus[k];
 })
 
-
-
-let resolution = 36; // how many points in the circle
-let rad = 150;
-let x = 1;
-let y = 1;
-//float prevX;
-//float prevY;
-
-let t = 0; // time passed
-let tChange = .02; // how quick time flies
 
 let nVal; // noise value
 let nInt = 190; // noise intensity
 let nAmp = 10; // noise amplitude
-function setup() {
-  let cnv = createCanvas(window.innerWidth, window.innerHeight);
-  cnv.position(0,0)
-  background(0);
-  stroke(255, 15);
-  noFill();
-  t = 0;
-  
+
+function setupColors() {
   let c1 = color("#00FF00");
   c1.setAlpha(15);
-   let c2 = color("#FFFF00");
+  let c2 = color("#FFFF00");
   c2.setAlpha(15);
-   let c3 = color("#FF0000");
+  let c3 = color("#FF0000");
   c3.setAlpha(15);
-  colors = [c1,c2,c3];
-  
+  colors = [c1, c2, c3];
+}
 
+
+
+'use strict';
+
+var tileCount = 5;
+var actRandomSeed = 0;
+
+var actStrokeCap;
+
+function setup() {
+  let cnv = createCanvas(600, 600);
+  cnv.position(window.innerWidth / 2 - 300, window.innerHeight / 2 - 300);
+  setupColors();
+
+
+  actStrokeCap = SQUARE;
 }
 
 function draw() {
-  translate(width/2, height/2);
-  
-  document.getElementById('decibel').textContent = dbString;
-    document.getElementById('status').textContent = status;
-
+  console.log(val);
   nInt = map(val, 0, 10.0, 2.0, 100.0);
   nAmp = map(val, 0, 10, 0.5, 400.0);
   cG = map(val, 0, 1.0, 255, 0);
- // stroke(255, cG, 0, val*15);
-  //console.log(cG);
-  stroke(colors[colorIndex]);
-  
-  beginShape();
-  for (var i = 0; i < 200; i++) {
-    var ang = map(i, 0, 200, 0, TWO_PI);
-    nVal = map(noise( cos(ang)*nInt+1, sin(ang)*nInt+1, t ), 0.0, 1.0, nAmp, 1.0); // map noise value to match the amplitude
-
-    x = cos(ang)*rad *nVal;
-    y = sin(ang)*rad *nVal;
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
-  t += tChange;
-
   // clear the background every 600 frames using mod (%) operator
   if (frameCount % 1200 == 0) {
-	background(0);
+    // background(0);
+    clear()
+
+  }
+  background(colors[colorIndex]);
+
+  strokeCap(actStrokeCap);
+
+  randomSeed(actRandomSeed);
+
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+
+      var posX = width / tileCount * gridX;
+      var posY = height / tileCount * gridY;
+
+      var toggle = int(random(0, 2));
+
+      if (toggle == 0) {
+        strokeWeight(nInt + 10.0);
+        stroke(0);
+
+        line(posX, posY, posX + width / tileCount, posY + height / tileCount);
+      }
+      if (toggle == 1) {
+        strokeWeight(nAmp + 10.0);
+        stroke(255);
+        line(posX, posY + width / tileCount, posX + height / tileCount, posY);
+      }
+
+      
+    }
   }
 }
+
+function mousePressed() {}
